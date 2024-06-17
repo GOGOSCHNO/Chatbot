e31dfee7bb2a8e9dd72750d072ceb0dbed25a054
GOGOSCHNO <157826855+GOGOSCHNO@users.noreply.github.com>
2024-06-17 21:55:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ document.getElementById('userInput').addEventListener('keydown', function(event)
 
 // Ajout de la fonction pour afficher le message d'accueil
 function displayWelcomeMessage() {
-    const welcomeMessage = "Welcome! I'm Paul, your dedicated virtual assistant for Puravive. How can I help you on your weight loss journey today? Whether you're curious about how our scientifically-backed formula works, need tips on integrating Puravive into your routine, or want to learn about our special offers, feel free to ask me anything. I'm here to guide and support you every step of the way!";
+    const welcomeMessage = "Welcome! I'm Paul, your dedicated virtual assistant for Puravive. How can I help you on your weight loss journey today?";
     const typingIndicator = showTypingIndicator();
     
     setTimeout(() => {
